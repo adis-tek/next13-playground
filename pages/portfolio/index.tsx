ac943c4f6eaabb04f426e19a0453f85c88d20e49
adis-tek <67308354+adis-tek@users.noreply.github.com>
2023-03-16 17:32:41 -0500

    get static props

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-function PortfolioPage() {
-  return <div>Portfolio page.</div>;
+export interface Props {
+  products: Products[];
+}
+export interface Products {
+  id: string;
+  title: string;
+}
+
+function PortfolioPage(props: Props) {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
 }
 
 export default PortfolioPage;
